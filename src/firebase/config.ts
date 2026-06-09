ea092dc3df8a2f30fa85ed8d10be8b6c6f442390
durgeshgupta119325-cell <durgeshgupta119325@gmail.com>
2026-06-09 08:04:44 +0530

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,14 @@
+import { initializeApp } from "firebase/app";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCby8FDcZnHGHKSkCnqG5xfkuHWQR-Yt4s",
-      authDomain: "studio-9506215785-8115c.firebaseapp.com",
-        projectId: "studio-9506215785-8115c",
-          storageBucket: "studio-9506215785-8115c.firebasestorage.app",
-            messagingSenderId: "594984144458",
-              appId: "1:594984144458:web:46d77b2944c2d928a7a0b3"
-              };
+  apiKey: "AIzaSyCby8FDCznHGHKSkCnqG5xkfkHWQA-Yt4s",
+  authDomain: "studio-9506215785-8115c.firebaseapp.com",
+  projectId: "studio-9506215785-8115c",
+  storageBucket: "studio-9506215785-8115c.appspot.com",
+  messagingSenderId: "594984144458",
+  appId: "1:594984144458:web:4Gd77b2944c2d928a7a0b3"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
